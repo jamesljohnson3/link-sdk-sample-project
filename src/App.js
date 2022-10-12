64fdd5bdbc550259ad55c8803d6325c4dd2c47f9
Max Clayton Clowes <contact@mcclowes.com>
2022-10-12 18:18:43 +0100

    [feat] styling, copy, and sections

--- src/App.js
+++ src/App.js
@@ -28,10 +28,30 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>link-sdk-sample-project</h1>
+
+        <p>Add a company ID below to begin linking financial data</p>
+
         <form>
           <label htmlFor="companyId">Company ID:</label>
           <input type="text" id="companyId" name="companyId" value={companyId} onChange={(e) => setCompanyId(e.target.value)}/>
         </form>
+
+        {
+          !complete && <button onClick={() => startConnecting()}>Start connecting</button>
+        }
+
+        <hr/>
+
+        <h4>Connection IDs</h4>
+
+        <p>As you establish valid connections, those connection IDs will appear here.</p>
+
+        {
+          connections.length >= 1
+          ? connections.map((id, i)=><div key={i}>{id}</div>)
+          : <div><i>No connections</i></div>
+        }
 
         {
           open && <div className="Modal">
@@ -45,18 +65,6 @@ const App = () => {
                 }}
               />
             </div>
-        }
-
-        {
-          !complete && <button onClick={() => startConnecting()}>Start connecting</button>
-        }
-
-        <h3>Connection IDs</h3>
-
-        {
-          connections.length >= 1
-          ? connections.map((id, i)=><div key={i}>{id}</div>)
-          : <div>No connections</div>
         }
       </header>
     </div>
